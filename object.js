26bc1da85d7a5bd3694a8d77204585dd38505e95
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-09-03 13:18:22 +0530

    two more problems done

--- object.js
+++ object.js
@@ -1,5 +1,5 @@
 // Problem-1
-
+// Access the golden rod color value in output.
 const colors = {
     red: "#ff0000",
     green: "#00ff00",
@@ -8,3 +8,30 @@ const colors = {
 };
 
 console.log(colors['golden rod']);
+
+
+// Task-2
+// For this object below add a property named passenger capacity with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+car.passengerCapacity = 5;
+console.log(car);
+
+// Task-3
+// Display the physics marks as output.
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+console.log(student.physics.marks);
