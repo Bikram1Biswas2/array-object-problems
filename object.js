4b6c0688f7453194245ef8fee4345d0256ff12e9
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-09-03 13:32:55 +0530

    two more problems done

--- object.js
+++ object.js
@@ -7,7 +7,7 @@ const colors = {
     "golden rod": '#daa520'
 };
 
-console.log(colors['golden rod']);
+// console.log(colors['golden rod']);
 
 
 // Task-2
@@ -20,7 +20,7 @@ const car = {
 };
 
 car.passengerCapacity = 5;
-console.log(car);
+// console.log(car);
 
 // Task-3
 // Display the physics marks as output.
@@ -34,4 +34,39 @@ const student = {
         marks: 30
     }
 };
-console.log(student.physics.marks);
+// console.log(student.physics.marks);
+
+// Task-4
+// Count the number of properties.
+
+let students = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+const propertyCount = Object.keys(students).length;
+console.log(propertyCount);
+
+// task-5 (Hard)
+// Loop through an object and print the key-value pairs with their types
+
+// Output:
+
+// key: name | type:  string
+// key: age | type:  number
+// key: city | type:  string
+// key: isStudent | type:  boolean
+
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+for(let key in myObject){
+    console.log(`key: ${key} | type : ${typeof(myObject[key])}`);
+}
